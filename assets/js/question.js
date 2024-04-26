c6c6e0c8deecbe0e611e43ceb98aa8dfdfb3f691
V2seon <lsj22210325@gmail.com>
2024-04-26 15:35:29 +0900

    푸시

--- assets/js/question.js
+++ assets/js/question.js
@@ -3,7 +3,7 @@ const imageFiles = [];
 
 // 이미지 파일명을 배열에 추가 (1.png부터 5.png까지)
 const startNumber = 1;
-const endNumber = 214;
+const endNumber = 215;
 
 for (let i = startNumber; i <= endNumber; i++) {
     const fileName = `/portfolio/images/question/${i}.png`;
